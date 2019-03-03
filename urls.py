66c78955e57b3c7085bca2cf540d2d7597ef6da5
Swetank Poddar <2424088p@student.gla.ac.uk>
2019-03-03 19:52:08 +0000

    Removing extra files

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from taskdesk import views
-
-
-urlpatterns = [
-	url(r'^login/$', views.user_login, name='login'),
-]
--- views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import visitor_cookie_handler
-
-def user_login(request):
-    return render(request, 'taskdesk/login.html',{})
